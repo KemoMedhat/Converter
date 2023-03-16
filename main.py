b8d34452f0fe5426ef3ab5b3a19adceb3fbc49bf
KemoMedhat <93602700+KemoMedhat@users.noreply.github.com>
2023-03-16 06:43:35 +0200

    khara

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import PySimpleGUI as sg   #importing the main lib and namming it as sg
-layout = [
-    [sg.Text('Text',enable_events=True,key='-txt-'),sg.Spin('item_1','item_2','item_3')],
-    [sg.Button('Button',key='-button1-')],
-    [sg.Input('hee',enable_events=True,key="-inp-")],
-    ]    # splitting the window to three columns and putting wideget or element in avery one of it
+layout = [[sg.Input(key='-input-',enable_events=True),sg.Spin(['KG-->G','M-->CM','GB-->MB','MG-->KB'],enable_events=True,key='-spn-')],
+          [sg.Text('    kareem      ',key='txt',enable_events=True),sg.Button('Convert',key='-btn-')]]
+
 window = sg.Window('Converter',layout)
 while True:
     event, value = window.read()
-    if event == sg.WIN_CLOSED:
-          break
-
-    
+    if event == sg.WIN_CLOSED :
+        break
+    if event == '-btn-' and value['-spn-'] == 'KG-->G':
+        window['txt'].update(value['-input-']+' KeloGrams = '+str(int(value['-input-'])*1000)+' Grams')
+    elif event == '-btn-' and value['-spn-'] == 'M-->CM':
+        window['txt'].update(value['-input-']+' Meter = '+str(int(value['-input-'])*1000)+' Centimeter')
+    elif event == '-btn-' and value['-spn-'] == 'GB-->MB':
+        window['txt'].update(value['-input-']+' Gigabytes = '+str(int(value['-input-'])*1024)+' Megabytes')
+    elif event == '-btn-' and value['-spn-'] == 'MG-->KB':
+        window['txt'].update(value['-input-']+' Megabytes = '+str(int(value['-input-'])*1024)+' Kelobytes')
+window.close()
