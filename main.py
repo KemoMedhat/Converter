1bf83cdd01dc4fcfca71d14f140d8fc2d592d611
KemoMedhat <93602700+KemoMedhat@users.noreply.github.com>
2023-03-16 05:36:10 +0200

    another commit

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-import PySimpleGUI as sg
+import PySimpleGUI as sg   #importing the main lib and namming it as sg
 layout = [
-    [sg.Text('Text'),sg.Button('OK')],
-    [sg.Button('Button')],
-    [sg.Input( )]
-]
-sg.Window('Converter',layout).read()
+    [sg.Text('Text',enable_events=True,key='-txt-'),sg.Spin('item_1','item_2','item_3')],
+    [sg.Button('Button',key='-button1-')],
+    [sg.Input('hee',enable_events=True,key="-inp-")],
+    ]    # splitting the window to three columns and putting wideget or element in avery one of it
+window = sg.Window('Converter',layout)
+conter = 0
+while True:
+    event, values = window.read()
 
+    if event == sg.WIN_CLOSED:
+        break
+
+    
