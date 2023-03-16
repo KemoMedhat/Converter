75f709206e3ec03d8b58a5ec42fb0a1827b97045
KemoMedhat <93602700+KemoMedhat@users.noreply.github.com>
2023-03-16 07:20:42 +0200

    need python 3.11

--- main.py
+++ main.py
@@ -5,15 +5,27 @@ layout = [[sg.Input(key='-input-',enable_events=True),sg.Spin(['KG-->G','M-->CM'
 
 window = sg.Window('Converter',layout)
 while True:
-    event, value = window.read()
+    event, values = window.read()
     if event == sg.WIN_CLOSED :
         break
-    if event == '-btn-' and value['-spn-'] == 'KG-->G':
-        window['txt'].update(value['-input-']+' KeloGrams = '+str(int(value['-input-'])*1000)+' Grams')
-    elif event == '-btn-' and value['-spn-'] == 'M-->CM':
-        window['txt'].update(value['-input-']+' Meter = '+str(int(value['-input-'])*1000)+' Centimeter')
-    elif event == '-btn-' and value['-spn-'] == 'GB-->MB':
-        window['txt'].update(value['-input-']+' Gigabytes = '+str(int(value['-input-'])*1024)+' Megabytes')
-    elif event == '-btn-' and value['-spn-'] == 'MG-->KB':
-        window['txt'].update(value['-input-']+' Megabytes = '+str(int(value['-input-'])*1024)+' Kelobytes')
+    input_values = values['-input-']
+    if event == '-btn-':
+        if input_values.isnumeric() :
+            # match values['-spn-'] :
+            #     case 'KG-->G' :
+            #        window['txt'].update(values['-input-']+' KeloGrams = '+str(int(values['-input-'])*1000)+' Grams')
+            #     case 'M-->CM' :
+            #         window['txt'].update(values['-input-']+' Meter = '+str(int(values['-input-'])*1000)+' Centimeter')
+            #     case 'GB-->MB':
+            #         window['txt'].update(values['-input-']+' Gigabytes = '+str(int(values['-input-'])*1024)+' Megabytes')
+            #     case 'MG-->KB' :
+            #         window['txt'].update(values['-input-']+' Megabytes = '+str(int(values['-input-'])*1024)+' Kelobytes')
+            if values['-spn-'] == 'KG-->G':
+               window['txt'].update(values['-input-']+' KeloGrams = '+str(int(values['-input-'])*1000)+' Grams')
+            elif values['-spn-'] == 'M-->CM':
+               window['txt'].update(values['-input-']+' Meter = '+str(int(values['-input-'])*1000)+' Centimeter')
+            elif values['-spn-'] == 'GB-->MB':
+               window['txt'].update(values['-input-']+' Gigabytes = '+str(int(values['-input-'])*1024)+' Megabytes')
+            elif values['-spn-'] == 'MG-->KB':
+               window['txt'].update(values['-input-']+' Megabytes = '+str(int(values['-input-'])*1024)+' Kelobytes')
 window.close()
